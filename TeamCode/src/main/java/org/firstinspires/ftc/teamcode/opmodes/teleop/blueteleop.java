eefe4882c191e1e743cf1b3cff3160ce63f9a1ae
Jinqi <jinqi.hu@renertschool.ca>
2026-04-09 11:34:53 -0600

    Shooter subsystem test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/blueteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/blueteleop.java
@@ -1,150 +1,76 @@
-package org.firstinspires.ftc.teamcode.opmodes.teleop; /*
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
-import static org.firstinspires.ftc.teamcode.subsystems.Calculations.findTPS;
-import static org.firstinspires.ftc.teamcode.subsystems.Calculations.findTPS44;
-import static org.firstinspires.ftc.teamcode.subsystems.Flywheel.shooter;*/
-
-import static org.firstinspires.ftc.teamcode.subsystems.ShooterSubsystem.shooter;
+import org.firstinspires.ftc.teamcode.subsystems.ShooterSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.drivesubsystem;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 import com.pedropathing.follower.Follower;
-import com.pedropathing.geometry.Pose;
-import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
-
-import org.firstinspires.ftc.teamcode.pedroPathing.Constants; /*
-import org.firstinspires.ftc.teamcode.subsystems.DistanceBlue;
-import org.firstinspires.ftc.teamcode.subsystems.DistanceRed;
-import org.firstinspires.ftc.teamcode.subsystems.DriveTrain;
-import org.firstinspires.ftc.teamcode.subsystems.TempHood;*/
-import org.firstinspires.ftc.teamcode.subsystems.drivesubsystem;
-
-
-import dev.nextftc.core.commands.delays.Delay;
-import dev.nextftc.core.commands.groups.SequentialGroup;
-import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.extensions.pedro.PedroComponent;
-import dev.nextftc.ftc.ActiveOpMode;
-import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
 import dev.nextftc.ftc.components.BulkReadComponent;
 import dev.nextftc.hardware.impl.MotorEx;
-import dev.nextftc.hardware.powerable.SetPower;
-
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "blue teleop")
 public class blueteleop extends NextFTCOpMode {
 
     public MotorEx intakeMotor;
     public MotorEx transferMotor;
+
     public blueteleop() {
         addComponents(
                 new PedroComponent(Constants::createFollower),
-                new SubsystemComponent(drivesubsystem.INSTANCE/*, Intake.INSTANCE, Spindexer.INSTANCE*/),
+                // Added ShooterSubsystem.INSTANCE here so its periodic() loop runs
+                new SubsystemComponent(drivesubsystem.INSTANCE, ShooterSubsystem.INSTANCE),
                 BulkReadComponent.INSTANCE,
                 BindingsComponent.INSTANCE
-
-
         );
     }
 
     public static boolean blue;
-    public static boolean isBlue(){
-        return blue;
-    }
+    public static boolean isBlue() { return blue; }
 
-    public static int tagID;
-    public static boolean findMotif = false;
-    public static int ball1Color = 0; //green = 1, purple = 2
-    public static int ball2Color = 0;
-    public static int ball3Color = 0;
-
-    public static int getBall1Color() {
-        return ball1Color;
-    }
-
-    public static int getBall2Color() {
-        return ball2Color;
-    }
-    public static int getBall3Color() {
-        return ball3Color;
-    }
-    public boolean lift;
-    boolean lowerangle = false;
-    public boolean liftmid;
-    boolean loweranglemid = false;
-
-
-
-
-    private static final int APRILTAG_PIPELINE = 8;
     @Override
     public void onInit() {
-//        Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
-//        limelight.pipelineSwitch(APRILTAG_PIPELINE);
-//        limelight.start();
-        blue=true;
+        blue = true;
         intakeMotor = new MotorEx("Intake_Motor");
         transferMotor = new MotorEx("Transfer_Motor");
-        Gamepads.gamepad1().leftTrigger().greaterThan(0.3).whenBecomesTrue(()-> intakeMotor.setPower(1))
+
+        // --- Shooter Controls ---
+        // Sets velocity to 2000 when A (Cross) is pressed
+        Gamepads.gamepad1().a().whenBecomesTrue(() -> ShooterSubsystem.INSTANCE.setVelocity(2000));
+
+        // Optional: Sets velocity to 0 when B (Circle) is pressed to stop the flywheels
+        Gamepads.gamepad1().b().whenBecomesTrue(() -> ShooterSubsystem.INSTANCE.setVelocity(0));
+
+        // --- Existing Intake/Transfer Controls ---
+        Gamepads.gamepad1().leftTrigger().greaterThan(0.3).whenBecomesTrue(() -> intakeMotor.setPower(1))
                 .whenBecomesFalse(() -> intakeMotor.setPower(0));
-        Gamepads.gamepad1().leftBumper().whenBecomesTrue(()-> transferMotor.setPower(1))
-                .whenBecomesFalse(() -> transferMotor.setPower(0));
-        /*Gamepads.gamepad2().leftTrigger().greaterThan(0.3).whenBecomesTrue(()->intakeMotor.setPower(-1))
-                .whenBecomesFalse(() -> intakeMotor.setPower(0));*/
-        Gamepads.gamepad1().rightTrigger().greaterThan(0.3).whenBecomesTrue(()-> transferMotor.setPower(-1))
+
+        Gamepads.gamepad1().leftBumper().whenBecomesTrue(() -> transferMotor.setPower(1))
                 .whenBecomesFalse(() -> transferMotor.setPower(0));
 
-
-
-
+        Gamepads.gamepad1().rightTrigger().greaterThan(0.3).whenBecomesTrue(() -> transferMotor.setPower(-1))
+                .whenBecomesFalse(() -> transferMotor.setPower(0));
     }
 
     @Override
     public void onUpdate() {
-        //float newtps=1000;
-        //shooter(newtps);
-        //ActiveOpMode.telemetry().addData("newtps", newtps);
-/*if(lowerangle==true){
-            newtps = findTPS44(DistanceBlue.INSTANCE.getDistanceFromTag());
-            //ActiveOpMode.telemetry().addData("Lowerangle:", lowerangle);
-        }
-        else if(lowerangle==false) {
-            newtps = findTPS(DistanceBlue.INSTANCE.getDistanceFromTag());
-            //ActiveOpMode.telemetry().addData("Lowerangle:", lowerangle);
-        }
-        if (DistanceBlue.INSTANCE.getDistanceFromTag() != 0) {
-            //shooter(newtps);
-            ActiveOpMode.telemetry().addData("newtps", newtps);
-        }*/
+        // Telemetry to track shooter speed
+        telemetry.addData("Target Velocity", 2000);
+        telemetry.addData("Left Flywheel Vel", ShooterSubsystem.flywheelvelocity);
+        telemetry.addData("Right Flywheel Vel", ShooterSubsystem.flywheelvelocity2);
     }
-
-    public boolean shoot;
 
     @Override
     public void onStartButtonPressed() {
-
-
-        //Gamepads.gamepad2().cross().whenBecomesTrue(() -> hood());
-        //Gamepads.gamepad2().triangle().whenBecomesTrue(() -> hoodMid());
-        /*SequentialGroup onStart= new SequentialGroup(
-                new Delay(2),
-                //TempHood.INSTANCE.HoodUp,
-                new SetPower(transferMotor, 0.25),
-                new Delay(0.01),
-                new SetPower(transferMotor, 0),
-                TempHood.INSTANCE.HoodUp,
-                new SetPower(transferMotor, 1),
-                new Delay(0.5),
-                TempHood.INSTANCE.HoodDown,
-                new SetPower(transferMotor, 0)
-        );
-        //int tag=MotifScanning.INSTANCE.findMotif();
-        onStart.schedule();*/
     }
 
-
-    public void onStop(){
-        blue=false;
+    public void onStop() {
+        blue = false;
+        // Safety: Stop shooter when OpMode stops
+        ShooterSubsystem.INSTANCE.setVelocity(0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSubsystem.java
@@ -1,89 +1,83 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-//import static org.firstinspires.ftc.teamcode.subsystems.Calculations.findTPS;
-
 import com.bylazar.configurables.annotations.Configurable;
-
 import dev.nextftc.bindings.*;
 import dev.nextftc.control.KineticState;
 import dev.nextftc.control.ControlSystem;
-
 import dev.nextftc.control.feedback.PIDCoefficients;
 import dev.nextftc.control.feedforward.BasicFeedforwardParameters;
 import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.ftc.ActiveOpMode;
 import dev.nextftc.hardware.impl.MotorEx;
+
 @Configurable
 public class ShooterSubsystem implements Subsystem {
-    public ShooterSubsystem() {
-
-    }
 
     public static final ShooterSubsystem INSTANCE = new ShooterSubsystem();
-    public static double flywheelvelocity;
 
-    public static double flywheelvelocity2;
-    private ControlSystem controller1;
-    private ControlSystem controller2;
-
+    // Hardware
     public static MotorEx flywheel = new MotorEx("LeftShooter_Motor");
-
     public static MotorEx flywheel2 = new MotorEx("RightShooter_Motor");
 
-    public static PIDCoefficients myPidCoeff = new PIDCoefficients(0.015, 0.005, 0.00);
-    public static BasicFeedforwardParameters myFF = new BasicFeedforwardParameters(0.00045, 0, 0.0);
+    // Static Controllers (Created once to preserve PID/Integral memory)
+    private static final ControlSystem controller1 = ControlSystem.builder()
+            .velPid(new PIDCoefficients(0.015, 0.005, 0.00))
+            .basicFF(new BasicFeedforwardParameters(0.00045, 0, 0.0))
+            .build();
 
+    private static final ControlSystem controller2 = ControlSystem.builder()
+            .velPid(new PIDCoefficients(0.015, 0.005, 0.00))
+            .basicFF(new BasicFeedforwardParameters(0.00045, 0, 0.0))
+            .build();
 
-    public static double configvelocity = 1400; //far zone - ~1500. near zone - ~1200-1300
+    public static double flywheelvelocity;
+    public static double flywheelvelocity2;
 
+    // Track the desired velocity across loops
+    private static float targetVelocity = 0;
+
+    /**
+     * Sets the desired velocity for the flywheels.
+     * The periodic loop will use PIDF to maintain this speed.
+     */
+    public void setVelocity(float tps) {
+        targetVelocity = tps;
+    }
+
+    // 1. Keep original name: Left side control
     public static void velocityControlWithFeedforward(KineticState currentstate, float configtps) {
-        // Create a velocity controller with PID and feedforward
-        ControlSystem controller1 = ControlSystem.builder()
-                .velPid(myPidCoeff) // Velocity PID with kP=0.1, kI=0.01, kD=0.05
-                .basicFF(myFF) // Basic feedforward with kV=0.02, kA=0.0, kS=0.01 //pid tuning
-                .build();
-
         controller1.setGoal(new KineticState(0.0, configtps, 0.0));
-
-        // In a loop (simulated here), you would:
-        // Create a KineticState with current position and velocity
-
         double power = controller1.calculate(currentstate);
         flywheel.setPower(power);
     }
+
+    // 2. Keep original name: Right side control
     public static void velocityControlWithFeedforward2(KineticState currentstate, float configtps) {
-        // Create a velocity controller with PID and feedforward
-        ControlSystem controller2 = ControlSystem.builder()
-                .velPid(myPidCoeff) // Velocity PID with kP=0.1, kI=0.01, kD=0.05
-                .basicFF(myFF) // Basic feedforward with kV=0.02, kA=0.0, kS=0.01 //pid tuning
-                .build();
-
         controller2.setGoal(new KineticState(0.0, configtps, 0.0));
-
-        // In a loop (simulated here), you would:
-        // Create a KineticState with current position and velocity
-
         double power = controller2.calculate(currentstate);
-        flywheel2.setPower(-1*power);
+        flywheel2.setPower(-power); // Inverted for hardware
     }
+
+    // 3. Keep original name: Main update loop
     public static void shooter(float tps) {
         BindingManager.update();
+
         flywheelvelocity = flywheel.getVelocity();
         flywheelvelocity2 = flywheel2.getVelocity();
-        KineticState currentState = new KineticState(0, flywheelvelocity, 0.0);
-        KineticState currentState2 = new KineticState(0, -1*flywheelvelocity2, 0.0);
-        velocityControlWithFeedforward(currentState, tps);
-        velocityControlWithFeedforward2(currentState2, tps);
-        double rpm = (flywheelvelocity / 28) * 60.0;
 
-    }
-    @Override public void initialize() {
-
+        // Pass current velocities to the controllers
+        velocityControlWithFeedforward(new KineticState(0, flywheelvelocity, 0), tps);
+        velocityControlWithFeedforward2(new KineticState(0, -flywheelvelocity2, 0), tps);
     }
 
-    @Override public void periodic() {
+    @Override
+    public void initialize() {
+        // Ensure motors are in a neutral state on start
+        targetVelocity = 0;
+    }
 
+    @Override
+    public void periodic() {
+        // Continuously run the shooter logic at the current target velocity
+        shooter(targetVelocity);
     }
 }
