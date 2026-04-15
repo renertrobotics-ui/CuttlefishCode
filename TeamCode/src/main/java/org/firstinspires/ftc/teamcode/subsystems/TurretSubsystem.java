6f3efd47d005d834e497312c3c272a9a6fe9b867
supersoimon <simon.gidrewicz@gmail.com>
2026-04-15 10:41:21 -0600

    WHAHWHHDSFAKWEHROIPAWEHGT;QWUIHPQWEIJGBQOIWREFBPQIWYEF FIANLLY TGUNENDOAISNE[UWAH4TPIQUWETPUQWHEGPIQUW4HGWI4OIH

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TurretSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TurretSubsystem.java
@@ -44,7 +44,7 @@ public class TurretSubsystem implements Subsystem {
 
     public static double RawEncoderValue;
     public double PreviousTurretPos;
-    public static PIDCoefficients myPidCoeff = new PIDCoefficients(0.00006, 0, 0.0003);
+    public static PIDCoefficients myPidCoeff = new PIDCoefficients(0.00006, 0, 0.0001);
 //    public static BasicFeedforwardParameters myFF = new BasicFeedforwardParameters(0.0, 0, 0.0);
 
 
@@ -98,12 +98,12 @@ public class TurretSubsystem implements Subsystem {
                 .posPid(myPidCoeff) // Velocity PID with kP=0.1, kI=0.01, kD=0.05
                 .build();
         controller2.setGoal(new KineticState(target, 0, 0));
-        if (Math.abs(target-turretpos) < 500) {
-            f = Math.signum(target-turretpos) * Math.abs(target-turretpos) / 500;
+        if (Math.abs(target-turretpos) == 0) {
+            f = 0;
         } else {
             f = Math.signum(target-turretpos);
         }
-        double turretF = 0.115;
+        double turretF = 0.076;
         ActiveOpMode.telemetry().addData("controller2.calculate(currentstate)", controller2.calculate(currentstate));
         double power = controller2.calculate(currentstate) + turretF*f;
         INSTANCE.PreviousTurretPos = turretpos;
